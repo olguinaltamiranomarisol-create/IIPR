2b2f2cb10e8947fbc53cdb5db01bca0306dd892f
olguinaltamiranomarisol-create <olguinaltamiranomarisol@gmail.com>
2026-04-21 18:43:50 -0600

    Add files via upload

---
+++ html.php
@@ -0,0 +1,30 @@
+html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro de Alumno</title>
+</head>
+<body>
+
+    <h2>Registro de Alumno</h2>
+    <form>
+        <label>Nombre del Alumno:</label><br>
+        <input type="text" name="nombre"><br><br>
+
+        <label>Edad:</label><br>
+        <input type="number" name="edad"><br><br>
+
+        <label>Escuela:</label><br>
+        <input type="text" name="escuela"><br><br>
+
+        <label>Número de Teléfono:</label><br>
+        <input type="tel" name="telefono"><br><br>
+
+        <button type="submit">Guardar Registro</button>
+    </form>
+
+</body>
+</html>
+
+  
+
