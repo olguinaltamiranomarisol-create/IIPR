181e47d60ef54273ef30d3c2a49c65b27b2e9685
olguinaltamiranomarisol-create <olguinaltamiranomarisol@gmail.com>
2026-05-12 18:36:25 -0600

    Add files via upload

---
+++ #Contados de pares.py
@@ -0,0 +1,9 @@
+#Contados de pares
+#Olguin Marisol
+
+par = 0 
+
+for i in range(1,51):
+    if i  %2 == 0:
+        par =part1
+        print("los pares son:",par)
---
+++ x = 0.py
@@ -0,0 +1,5 @@
+x = 0
+
+for num in range(1, 11):
+    x = x + num
+    print(x)
