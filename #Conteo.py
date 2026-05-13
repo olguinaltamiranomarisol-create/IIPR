2642bec47f707a40198c794907e39d66affee110
olguinaltamiranomarisol-create <olguinaltamiranomarisol@gmail.com>
2026-05-12 18:40:25 -0600

    Add files via upload

---
+++ #Conteo.py
@@ -0,0 +1,11 @@
+#Conteo
+#Olguin Marisol
+ 
+suma = 0
+ 
+for i in range (1,11):
+suma = suma + 1
+ 
+print(suma)
+ 
+print("Me doy de baja mejor")
---
+++ #Sorteo.py
@@ -0,0 +1,9 @@
+#Sorteo
+#Olguin Marisol
+ 
+original = [45, 12, 78, 3, 56]
+nueva = sorted(original)
+desc = sorted(original, reverdr=True)
+print('original:', original)
+print('Orden ascendente' , nueva)
+print('Orde')
